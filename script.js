d4b7baedc420447bacfe38de392134f984e6bd4e
hireahitman <159198420+hireahitman@users.noreply.github.com>
2024-11-18 19:55:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 // Toggle Mobile Navigation
 function toggleMenu() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-}
+   const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-// Smooth Scrolling for Links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');   
+
 });
